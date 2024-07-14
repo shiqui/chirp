e857999d8c401d18405a2a50e2d86ebe2a87f37d
Shi Qi <shiqi8sq@gmail.com>
2024-07-14 15:57:10 -0400

    Get rid of feed scrollbar

--- src/components/home/feed.tsx
+++ src/components/home/feed.tsx
@@ -4,7 +4,7 @@ import { Post } from "./post";
 export const Feed = async () => {
   const posts = await getAllPosts();
   return (
-    <div className="flex flex-col overflow-y-auto scrollbar-thin scrollbar-track-slate-400 scrollbar-thumb-slate-700 scrollbar-thumb-rounded-full">
+    <div className="flex flex-col overflow-y-auto">
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,10 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("tailwind-scrollbar")({ nocompatible: true }),
-  ],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
