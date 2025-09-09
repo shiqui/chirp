785a6f46fe8f84843b1ae4b9eb4724142004ceb7
shiqui <shiqi8sq@gmail.com>
2025-09-09 03:29:06 -0400

    Fix emoji regex

--- src/db/actions/post.ts
+++ src/db/actions/post.ts
@@ -43,7 +43,8 @@ export const createPost = async (
     };
   }
 
-  const emojiRegex = /^[\p{Emoji}\s\n\r]+$/u;
+  const emojiRegex =
+    /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
   if (!emojiRegex.test(content)) {
     return {
       success: false as const,
